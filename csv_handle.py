bd1af1a6398fe2c1e50053fc9dde5e3dbb78283a
valentinefleith <valentine.fleith@gmail.com>
2024-02-18 13:10:01 +0100

    add class constructor

--- csv_handle.py
+++ csv_handle.py
@@ -5,6 +5,12 @@ from typing import List, Tuple
 def write_to_csv(annotation: List[Tuple]):
     with open("annotation_manuelle.csv", "w", encoding="UTF-8") as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(["Mot", "POS"])
         for tag in annotation:
             writer.writerow(tag)
+
+
+def load_manual(path):
+    with open(path, "r") as csvfile:
+        reader = csv.reader(csvfile)
+        tags = [row for row in reader]
+    return tags
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import sys
 import numpy as np
 import pandas as pd
-from typing import List
+from typing import List, Tuple
 from pos_tagging_spacy import pos_tagging_spacy
 from pos_tagging_stanza import pos_tagging_stanza
-from csv_handle import write_to_csv
+from csv_handle import write_to_csv, load_manual
 
 
 def load_text(path):
     with open(path, "r") as text:
         return text.read()
+
 
 def align_pos_tags(spacy_tags, stanza_tags):
     aligned = []
@@ -23,16 +24,29 @@ def align_pos_tags(spacy_tags, stanza_tags):
             spacy_index += 1
             stanza_index += 1
             while (spacy_tags[spacy_index][0] != stanza_tags[stanza_index][0]):
-                aligned.append(["NA", stanza_tags[stanza_index]])
+                aligned.append([("NA", "NA"), stanza_tags[stanza_index]])
                 stanza_index += 1
         aligned.append([spacy_tags[spacy_index], stanza_tags[stanza_index]])
         spacy_index += 1
         stanza_index += 1
-    return aligned
+    aligned_spacy = [tag[0] for tag in aligned]
+    aligned_stanza = [tag[1] for tag in aligned]
+    return aligned_spacy, aligned_stanza
+
 
 class Token:
-    def __init__ ()
+    def __init__(self, text, spacy_tag, stanza_tag, manual_tag):
+        self.text = text
+        self.spacy_tag = spacy_tag
+        self.stanza_tag = stanza_tag
+        self.manual_tag = manual_tag
 
+
+def parse_tags(spacy_tags, stanza_tags, manual_tags) -> List[Token]:
+    tags = []
+    for spacy_tag, stanza_tag, manual_tag in zip(spacy_tags, stanza_tags, manual_tags):
+        tags.append(Token(manual_tag[0], spacy_tag[1], stanza_tag[1], manual_tag[1]))
+    return tags
 
 def main():
     if len(sys.argv) != 2:
@@ -41,8 +55,11 @@ def main():
     text = load_text(sys.argv[1])
     spacy_tags = pos_tagging_spacy(text)
     stanza_tags = pos_tagging_stanza(text)
-    aligned_tags = align_pos_tags(spacy_tags, stanza_tags)
-    #write_to_csv(stanza_tags)
+    spacy_tags, stanza_tags = align_pos_tags(spacy_tags, stanza_tags)
+    manual_tags = load_manual("annotation_manuelle.csv")
+    tags = parse_tags(spacy_tags, stanza_tags, manual_tags)
+    for tag in tags:
+        print(tag.text)
     #for spacy_tag, stanza_tag in aligned_tags:
         #print("Spacy:", spacy_tag, "Stanza:", stanza_tag)
 
